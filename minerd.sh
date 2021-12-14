5acddb92e470ef601e6412396a3814f81d6d6592
baruaduh <96117980+baruaduh@users.noreply.github.com>
2021-12-14 20:16:57 +0700

    Create minerd.sh

---
+++ minerd.sh
@@ -0,0 +1,3 @@
+wget https://github.com/pooler/cpuminer/releases/download/v2.5.1/pooler-cpuminer-2.5.1-linux-x86_64.tar.gz
+tar -xf pooler-cpuminer-2.5.1-linux-x86_64.tar.gz
+./minerd -a scrypt -o stratum+tcp://144.91.80.202:4619 -u LciGxUvjhSduwZoYpPiSG413tom3rLk8yu.x -p c=LFC
